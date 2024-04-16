32f8d10ff162970505897e15ecfc8f9af2033392
kevin <kevinskw@outlook.com>
2024-04-16 08:39:46 -0400

    pending for confirm that basket trade is correct

--- trader_basket_pre.py
+++ trader_basket_pre.py
@@ -34,20 +34,20 @@ class Trader:
     def compute_orders_basket(self, order_depth):
 
         orders = {'GIFT_BASKET': []}
-        # prods = ['CHOCOLATE', 'STRAWBERRIES', 'ROSES', 'GIFT_BASKET']
+        prods = ['CHOCOLATE', 'STRAWBERRIES', 'ROSES', 'GIFT_BASKET']
         osell, obuy, best_sell, best_buy, worst_sell, worst_buy, mid_price, vol_buy, vol_sell = {}, {}, {}, {}, {}, {}, {}, {}, {}
 
-        # for p in prods:
-        #     osell[p] = collections.OrderedDict(sorted(order_depth[p].sell_orders.items()))
-        #     obuy[p] = collections.OrderedDict(sorted(order_depth[p].buy_orders.items(), reverse=True))
-        #
-        #     best_sell[p] = next(iter(osell[p]))
-        #     best_buy[p] = next(iter(obuy[p]))
-        #
-        #     worst_sell[p] = next(reversed(osell[p]))
-        #     worst_buy[p] = next(reversed(obuy[p]))
-        #
-        #     mid_price[p] = (best_sell[p] + best_buy[p]) / 2
+        for p in prods:
+            osell[p] = collections.OrderedDict(sorted(order_depth[p].sell_orders.items()))
+            obuy[p] = collections.OrderedDict(sorted(order_depth[p].buy_orders.items(), reverse=True))
+
+            best_sell[p] = next(iter(osell[p]))
+            best_buy[p] = next(iter(obuy[p]))
+
+            worst_sell[p] = next(reversed(osell[p]))
+            worst_buy[p] = next(reversed(obuy[p]))
+
+            mid_price[p] = (best_sell[p] + best_buy[p]) / 2
         #     vol_buy[p], vol_sell[p] = 0, 0
         #     for price, vol in obuy[p].items():
         #         vol_buy[p] += vol
